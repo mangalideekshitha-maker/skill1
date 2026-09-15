07f53f91f7c9601c2cfeea6029d9344506233c18
Mangali Deekshitha <mangali.deekshitha@klh.edu.in>
2026-09-15 06:16:35 +0000

    Add Practical 5

--- Makefile
+++ Makefile
@@ -6,7 +6,8 @@ prog1: prog1.c
 
 prog2: prog2.c
 	$(CC) $(CFLAGS) -o prog2 prog2.c
-
+prog5: prog5.c
+	gcc prog5.c -o prog5
 clean:
 	rm -f prog1 prog2
 
---
+++ prog5.c
@@ -0,0 +1,165 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <time.h>
+
+#define NUM_MESSAGES 10000
+#define MESSAGE_SIZE 100
+
+int main()
+{
+    int pipefd[2];
+    pid_t pid;
+
+    char message[MESSAGE_SIZE];
+    char buffer[MESSAGE_SIZE];
+
+    struct timespec start, end;
+
+    printf("===== PRODUCER-CONSUMER USING PIPE =====\n");
+
+    if (pipe(pipefd) == -1)
+    {
+        perror("pipe");
+        exit(EXIT_FAILURE);
+    }
+
+    pid = fork();
+
+    if (pid < 0)
+    {
+        perror("fork");
+        exit(EXIT_FAILURE);
+    }
+
+    /* Child Process - Consumer */
+    if (pid == 0)
+    {
+        close(pipefd[1]);
+
+        int count = 0;
+
+        while (read(pipefd[0], buffer, MESSAGE_SIZE) > 0)
+        {
+            count++;
+        }
+
+        close(pipefd[0]);
+
+        printf("Consumer: Received %d messages\n", count);
+
+        exit(0);
+    }
+
+    /* Parent Process - Producer */
+    else
+    {
+        close(pipefd[0]);
+
+        clock_gettime(CLOCK_MONOTONIC, &start);
+
+        for (int i = 0; i < NUM_MESSAGES; i++)
+        {
+            snprintf(message, MESSAGE_SIZE,
+                     "Message %d from Producer", i + 1);
+
+            if (write(pipefd[1], message, MESSAGE_SIZE) == -1)
+            {
+                perror("write");
+                exit(EXIT_FAILURE);
+            }
+        }
+
+        close(pipefd[1]);
+
+        wait(NULL);
+
+        clock_gettime(CLOCK_MONOTONIC, &end);
+
+        double elapsed =
+            (end.tv_sec - start.tv_sec) +
+            (end.tv_nsec - start.tv_nsec) / 1e9;
+
+        long total_bytes =
+            (long)NUM_MESSAGES * MESSAGE_SIZE;
+
+        double throughput =
+            total_bytes / elapsed / (1024 * 1024);
+
+        printf("Producer: Sent %d messages\n", NUM_MESSAGES);
+        printf("Total data transferred : %ld bytes\n", total_bytes);
+        printf("Communication time : %.6f seconds\n", elapsed);
+        printf("Communication throughput: %.2f MB/s\n",
+               throughput);
+    }
+
+    printf("\n===== LS -L | GREP \".C\" USING PIPE =====\n");
+
+    int pipe2[2];
+    pid_t pid1, pid2;
+
+    if (pipe(pipe2) == -1)
+    {
+        perror("pipe");
+        exit(EXIT_FAILURE);
+    }
+
+    /* First child - ls -l */
+    pid1 = fork();
+
+    if (pid1 < 0)
+    {
+        perror("fork");
+        exit(EXIT_FAILURE);
+    }
+
+    if (pid1 == 0)
+    {
+        close(pipe2[0]);
+
+        dup2(pipe2[1], STDOUT_FILENO);
+
+        close(pipe2[1]);
+
+        execlp("ls", "ls", "-l", (char *)NULL);
+
+        perror("execlp ls");
+        exit(EXIT_FAILURE);
+    }
+
+    /* Second child - grep ".c" */
+    pid2 = fork();
+
+    if (pid2 < 0)
+    {
+        perror("fork");
+        exit(EXIT_FAILURE);
+    }
+
+    if (pid2 == 0)
+    {
+        close(pipe2[1]);
+
+        dup2(pipe2[0], STDIN_FILENO);
+
+        close(pipe2[0]);
+
+        execlp("grep", "grep", ".c", (char *)NULL);
+
+        perror("execlp grep");
+        exit(EXIT_FAILURE);
+    }
+
+    /* Parent */
+    close(pipe2[0]);
+    close(pipe2[1]);
+
+    waitpid(pid1, NULL, 0);
+    waitpid(pid2, NULL, 0);
+
+    printf("\nPipeline execution completed.\n");
+
+    return 0;
+}
